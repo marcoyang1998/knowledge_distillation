47eed6758621b532c6474d001f655e7c08ac07a9
xiaoyu <marcoyang1998@gmail.com>
2022-09-13 01:12:44 +0800

    add necessary files for ctc bpe training

---
+++ egs/librispeech/asr1/run_wavlm_ctc.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+. ./path.sh || exit 1;
+. ./cmd.sh || exit 1;
+
+# general configuration
+backend=pytorch
+ngpu=8         # number of gpus ("0" uses cpu, otherwise use gpu)
+debugmode=1
+dumpdir=dump   # directory to dump full features
+verbose=0      # verbose option
+
+# feature configuration
+do_delta=false
+
+train_config=
+decode_config=conf/decode.yaml
+
+. utils/parse_options.sh || exit 1;
+
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+train_set=librispeech_clean_100
+data_dir=waveform_training_data
+
+model=$1
+exptag=$2
+nbpe=256
+expname=train_${train_set}_${model}_${exptag}
+expdir=exp_wavlm_bpe/${expname}
+echo "exp dir: ${expdir}" 
+mkdir -p ${expdir}
+
+train_dir=${data_dir}/train_clean_100_shortened
+valid_dir=${data_dir}/dev_other_100
+dict=data/lang_char/train_clean_100_unigram${nbpe}_units.txt
+echo "dictionary: ${dict}"
+
+resume=
+
+echo "Network Training"
+    ${cuda_cmd} --gpu ${ngpu} ${expdir}/train.log \
+        asr_train.py \
+        --config ${train_config} \
+        --ngpu ${ngpu} \
+        --backend ${backend} \
+        --outdir ${expdir}/results \
+        --tensorboard-dir tensorboard/${expname} \
+        --debugmode ${debugmode} \
+        --dict ${dict} \
+        --debugdir ${expdir} \
+        --minibatches ${N} \
+        --verbose ${verbose} \
+        --resume ${resume} \
+        --train-json ${train_dir}/data_unigram256.json \
+        --valid-json ${valid_dir}/data_unigram256_truncated_100.json
